24b873ff85ff1f71129a10ed81efe63cd34eb00b
Kishore Kumar <kishore.kumar@pax-ai.ae>
2020-06-03 20:56:24 +0400

    indentation

--- OctagonAnalytics/ViewController/Charts/PieChartViewController.swift
+++ OctagonAnalytics/ViewController/Charts/PieChartViewController.swift
@@ -57,8 +57,8 @@ class PieChartViewController: ChartBaseViewController {
         let top = NSLayoutConstraint(item: chartVC.view!, attribute: .top, relatedBy: .equal, toItem: displayContainerView, attribute: .top, multiplier: 1.0, constant: 0.0)
         let left = NSLayoutConstraint(item: chartVC.view!, attribute: .left, relatedBy: .equal, toItem: displayContainerView, attribute: .left, multiplier: 1.0, constant: 0.0)
         let bottom = NSLayoutConstraint(item: chartVC.view!, attribute: .bottom, relatedBy: .equal, toItem: displayContainerView, attribute: .bottom, multiplier: 1.0, constant: 0.0)
-        let right = NSLayoutConstraint(item: chartVC.view!, attribute: .right, relatedBy: .equal, toItem: displayContainerView, attribute: .right, multiplier: 1.0, constant: 0.0)
-        view.addConstraints([top, left, right, bottom])
+        let width = NSLayoutConstraint(item: chartVC.view!, attribute: .width, relatedBy: .equal, toItem: displayContainerView, attribute: .height, multiplier: 1.0, constant: 0.0)
+        view.addConstraints([top, left, width, bottom])
         chartVC.view.translatesAutoresizingMaskIntoConstraints = false
         chartProvider.onSelect = nodeSelected
         chartProvider.onDeselect = {
@@ -85,34 +85,8 @@ class PieChartViewController: ChartBaseViewController {
     }
 }
 
-extension PieChartViewController: ChartViewDelegate {
-    func chartValueSelected(_ chartView: ChartViewBase, entry: ChartDataEntry, highlight: Highlight) {
-        DLog("Chart Value Selected")
-        
-        guard let chartContent = entry.data as? ChartContent,
-            let agg = panel?.bucketAggregation else { return }
-      
-        var dateComponant: DateComponents?
-        if let selectedDates =  panel?.currentSelectedDates,
-            let fromDate = selectedDates.0, let toDate = selectedDates.1 {
-            dateComponant = fromDate.getDateComponents(toDate)
-        }
-        let filter = FilterProvider.shared.createFilter(chartContent, dateComponents: dateComponant, agg: agg)
-        
-        showInfoFieldActionBlock?(self, [filter], nil)
-    }
-    
-    func chartValueNothingSelected(_ chartView: ChartViewBase) {
-        DLog("Chart Selection cleared")
-        chartView.highlightValues(nil)
-        deselectFieldAction?(self)
-    }
-}
-
-
 extension PieChartViewController {
     func getPieChartData() -> [PieChartNode] {
-        
         guard let parsedAgg = panel?.parsedAgg else {
             return []
         }
