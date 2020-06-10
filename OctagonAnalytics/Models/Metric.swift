620405cdceb8387e1d26f846cc9db471c09150b9
Kishore Kumar <kishore.kumar@pax-ai.ae>
2020-06-10 12:24:52 +0400

    Metrcis- Buckets Support

--- OctagonAnalytics/Models/Metric.swift
+++ OctagonAnalytics/Models/Metric.swift
@@ -10,10 +10,39 @@ import UIKit
 import ObjectMapper
 
 class Metric: Mappable {
-
+    var id: String = ""
     var type: String    = ""
     var value: NSNumber  = NSNumber(value: 0)
-    var label: String   = ""
+    private var labelStr: String?
+    private var labelInt: Int?
+    var label: String {
+        if let str = labelStr, !str.isEmpty {
+            return str
+        } else if let int = labelInt, int > 0 {
+            return String(int)
+        } else {
+            return ""
+        }
+    }
+    weak var panel: MetricPanel?
+    
+    var computedLabel: String {
+        var computed = label
+        let filteredMetric = panel?.visState?.metricAggregationsArray.filter { $0.id == id }
+
+        guard let metric = filteredMetric?.first,
+            panel?.visState?.otherAggregationsArray.isEmpty == false else {
+            return computed
+        }
+        
+        computed += "-" + metric.metricType.displayValue
+        
+        if metric.field.isEmpty == false {
+            computed += " " + metric.field
+        }
+        
+        return computed
+    }
     
     //MARK: Functions
     required init?(map: Map) {
@@ -21,8 +50,10 @@ class Metric: Mappable {
     }
     
     func mapping(map: Map) {
+        id              <- map[MetricConstant.id]
         type            <- map[MetricConstant.type]
-        label           <- map[MetricConstant.label]
+        labelStr        <- map[MetricConstant.label]
+        labelInt        <- map[MetricConstant.label]
         value           <- map[MetricConstant.value]
     }
 
