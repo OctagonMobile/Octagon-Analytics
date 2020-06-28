a1c8e6cd22dba14f87de8767416f92ef4c653f97
rameez <rmz.rmz@live.com>
2020-06-28 15:03:29 +0400

    Minor ui fixes

--- OctagonAnalytics/CellsAndViews/VideoGenerator/RecordButton.swift
+++ OctagonAnalytics/CellsAndViews/VideoGenerator/RecordButton.swift
@@ -10,14 +10,31 @@ import UIKit
 
 class RecordButton: UIButton {
     
+    private var fromColor: UIColor = UIColor.colorFromHexString("FF453A")
+    private var toColor: UIColor = UIColor.colorFromHexString("FF3B30")
+
+    //MARK: Functions
     override init(frame: CGRect) {
         super.init(frame: frame)
         setImage(UIImage(named: "Record"), for: .normal)
-        layer.backgroundColor = UIColor.red.cgColor
+        layer.backgroundColor = fromColor.cgColor
         layer.cornerRadius = frame.height / 2
+        animateBackground()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func animateBackground() {
+
+        let colourAnim = CABasicAnimation(keyPath: "backgroundColor")
+        colourAnim.repeatCount = .infinity
+        colourAnim.fromValue = fromColor.cgColor
+        colourAnim.toValue = toColor.cgColor
+        colourAnim.duration = 1.0
+        colourAnim.fillMode = .forwards
+        colourAnim.isRemovedOnCompletion = false
+        layer.add(colourAnim, forKey: "colourAnimation")
     }
 }
